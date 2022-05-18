a75952b5e37fe1c9aad4562c29a581201c96f9f0
Matheus-Reinert <reinert.matheus@hotmail.com>
2022-05-17 21:45:32 -0300

    Adicionado movimentos possíveis e pontos iniciais e finais

--- src/Mouse.java
+++ src/Mouse.java
@@ -1,71 +1,69 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+
+
+/*  E = saída 
+    # = parede
+    C = queijo
+    S = entrada 
+    M = mouse */
 
 public class Mouse {
 
-    static boolean isSafe(int[][] arr, int row, int col) {
-        return(row < arr.length && col < arr.length && col>=0 && row>=0 && arr[row][col] == 1);            
+    private static Map<String, Integer> findRownsAndColunsToDescoveryStartAndExitPoint(String[][] maze) {
+        Map<String, Integer> result = new HashMap<>(); 
+
+        int originRow = 0;
+        int originColumn = 0;
+        int destineRow = 0;
+        int destColumn = 0;
+
+        for (int i = 0; i < maze.length; i++) {
+            for (int j = 0; j < maze[1].length; j++) {
+                if (maze[i][j].equals("E")){
+                    originRow = i;
+                    originColumn = j;
+                }
+                if (maze[i][j].equals("S")){
+                    destineRow = i;
+                    destColumn = j;
+                }
+            }
+        }
+
+        result.put("originRow", originRow);
+        result.put("originColumn", originColumn);
+        result.put("destineRow", destineRow);
+        result.put("destColumn", destColumn);
+
+        return result;
     }
 
-    static boolean findPathUtil(int[][] arr, int[][] sol, int row, int col) {
-
-        if(row == arr.length - 1 && col == arr.length - 1 && arr[row][col] == 1) {
-            sol[row][col] = 1;
-            return true;
-        }
-
-        if(isSafe(arr, row, col)) {
-            sol[row][col] = 1;
-
-            if(findPathUtil(arr, sol, row + 1, col )) {
-                return true;
-            }
-
-            if(findPathUtil(arr, sol, row, col + 1)) {
-                return true;
-            }
-
-            sol[row][col] = 0;
-            return false;
-        }
-
-        return false;   
-    }    
-    
-    static void findPath(int[][] arr, int n) {
-        int[][] solution = new int[n][n];
+    private static void findWay(String[][] maze, int originRow, int originColumn) {
+        maze[originRow][originColumn] = "M";
         
-        if(findPathUtil(arr, solution, 0, 0)) {
-            printArray(solution);     
-        } else {
-            System.out.println("Sem solução");
-        }
-    }
 
-    static void printArray(int[][] arr) {
-        for(int i = 0; i < arr.length; i ++) {
-            for(int j = 0; j < arr.length; j++){
-                System.out.print(arr[i][j] + "  ");
+        
+
+    }
+    
+    private static void printMaze(String maze[][]) {
+        for(int i = 0; i < maze.length; i ++) {
+            for(int j = 0; j < maze[1].length; j++){
+                System.out.print(maze[i][j]);
             }
-            System.out.println();
+            System.out.println(); 
         }
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
-        int maze[][] = {{ 1, 1, 0, 0 },
-                        { 1, 1, 0, 0 },
-                        { 1, 1, 0, 0 },
-                        { 0, 1, 1, 1 }};        
-
-        int n = maze.length;    
-        findPath(maze, n);
-    }
-
-
-/*     Scanner scanner = new Scanner(new File("C:\\maze.txt"));
+    private static String[][] createMaze() throws FileNotFoundException {
+        
+        Scanner scanner = new Scanner(new File("C:\\maze.txt"));
         List<String []> list = new ArrayList<>();
 
         while (scanner.hasNext()) {
@@ -74,13 +72,53 @@ public class Mouse {
             for (int i = 0; i<ar.length; i++) parsed[i] = String.valueOf(ar[i]);
             list.add(parsed);
         }
+        
+        String[][]maze = list.toArray(new String[0][]);
 
-        String[][]matrix = list.toArray(new String[0][]);
+        return maze; 
+    }
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[1].length; j++) {
-                System.out.print(matrix[i][j]);
+    public static void main(String[] args) throws FileNotFoundException {
+
+        String maze[][] = createMaze(); 
+        /* printMaze(maze);              
+        Map<String, Integer> rowsAndColumns = findRownsAndColunsToDescoveryStartAndExitPoint(maze);
+        int originRow = rowsAndColumns.get("originRow");
+        int originColumn = rowsAndColumns.get("originColumn"); */
+        discoveryPossibleMoves(maze);
+
+        /* findWay(maze, originRow, originColumn); */
+        
+      /*   int originRow = rowsAndColumns.get("originRow");
+        int originColumn = rowsAndColumns.get("originColumn");
+        int destineRow = rowsAndColumns.get("destineRow");
+        int destColumn = rowsAndColumns.get("destColumn"); */
+
+
+    }
+
+    private static boolean[][] discoveryPossibleMoves(String[][] maze) {
+        boolean[][] possibleMoves = new boolean[maze.length][maze[1].length];
+
+        for(int i = 0; i < maze.length; i ++) {
+            for(int j = 0; j < maze[1].length; j++){
+                if(maze[i][j].equals("#")) {
+                   possibleMoves[i][j] = false;
+                } else {
+                    possibleMoves[i][j] = true;
+                }
             }
-            System.out.println();
+        }
+
+        /* System.out.println("Possible moves");
+
+        for(int i = 0; i < possibleMoves.length; i ++) {
+            for(int j = 0; j < possibleMoves[1].length; j++){
+                System.out.print(" " + possibleMoves[i][j] + " ");
+            }
+            System.out.println(); 
         } */
+        return possibleMoves;        
+    }
+
 }
